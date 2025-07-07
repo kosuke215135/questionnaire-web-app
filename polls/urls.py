541cef652ba6b58119ccbc3f865214e11a03b044
ichi <e215402@ie.u-ryukyu.ac.jp>
2025-07-07 16:09:56 +0900

    add: survey results page を追加

--- polls/urls.py
+++ polls/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .views import SurveyDetailView, SurveyResultsView, survey_vote
 
 __all__ = ['DetailView', 'ResultsView', 'vote']
 
@@ -7,4 +8,7 @@ urlpatterns = [
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('<int:pk>/vote/', views.vote, name='vote'),
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('survey/<int:pk>/', SurveyDetailView.as_view(), name='survey_detail'),
+    path('survey/<int:pk>/results/', SurveyResultsView.as_view(), name='survey_results'),
+    path('survey/<int:pk>/vote/', survey_vote, name='survey_vote'),
 ] 
--- sample_data.py
+++ sample_data.py
@@ -5,18 +5,46 @@ from django.utils import timezone
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 django.setup()
 
-from polls.models import Question, Choice
+from polls.models import Survey, Question, Choice
 
-# 仮の質問を作成
-q = Question.objects.create(
-    question_text="好きな飲み物は？",
+# サーベイ（アンケート）を作成
+survey = Survey.objects.create(
+    title="好きなものアンケート",
+    description="みんなの好みを調査します",
     pub_date=timezone.now()
 )  # type: ignore
 
-# 仮の選択肢を作成
-Choice.objects.create(question=q, choice_text="コーヒー", votes=0)  # type: ignore
-Choice.objects.create(question=q, choice_text="紅茶", votes=0)    # type: ignore
-Choice.objects.create(question=q, choice_text="水", votes=0)      # type: ignore
-Choice.objects.create(question=q, choice_text="ジュース", votes=0) # type: ignore
+# 1つ目の質問
+q1 = Question.objects.create(
+    survey=survey,
+    question_text="好きな飲み物は？",
+    pub_date=timezone.now()
+)  # type: ignore
+Choice.objects.create(question=q1, choice_text="コーヒー", votes=0)  # type: ignore
+Choice.objects.create(question=q1, choice_text="紅茶", votes=0)    # type: ignore
+Choice.objects.create(question=q1, choice_text="水", votes=0)      # type: ignore
+Choice.objects.create(question=q1, choice_text="ジュース", votes=0) # type: ignore
 
-print("サンプルデータ（飲み物アンケート）の投入が完了しました。") 
+# 2つ目の質問
+q2 = Question.objects.create(
+    survey=survey,
+    question_text="好きな季節は？",
+    pub_date=timezone.now()
+)  # type: ignore
+Choice.objects.create(question=q2, choice_text="春", votes=0)  # type: ignore
+Choice.objects.create(question=q2, choice_text="夏", votes=0)  # type: ignore
+Choice.objects.create(question=q2, choice_text="秋", votes=0)  # type: ignore
+Choice.objects.create(question=q2, choice_text="冬", votes=0)  # type: ignore
+
+# 3つ目の質問
+q3 = Question.objects.create(
+    survey=survey,
+    question_text="好きなペットは？",
+    pub_date=timezone.now()
+)  # type: ignore
+Choice.objects.create(question=q3, choice_text="犬", votes=0)  # type: ignore
+Choice.objects.create(question=q3, choice_text="猫", votes=0)  # type: ignore
+Choice.objects.create(question=q3, choice_text="鳥", votes=0)  # type: ignore
+Choice.objects.create(question=q3, choice_text="魚", votes=0)  # type: ignore
+
+print("サンプルデータ（アンケート＋複数質問）の投入が完了しました。") 
